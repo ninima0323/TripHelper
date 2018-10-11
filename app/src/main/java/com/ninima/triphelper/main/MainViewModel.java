f322174f8f4f1489832f408bd827da68cb28720d
raonian <raonian@webtoonscorp.com>
2018-10-11 12:14:50 +0900

    뷰모델 생성자 처리

--- app/src/main/java/com/ninima/triphelper/main/MainViewModel.java
+++ app/src/main/java/com/ninima/triphelper/main/MainViewModel.java
@@ -9,10 +9,14 @@ import com.ninima.triphelper.model.Trip;
 //뷰모델은 무조건 public 접근제한자 필요
 public class MainViewModel extends ViewModel {
     private TripDao tripDao =  MyDatabase.instance().tripDao();
-
-    LiveData<Trip> trip = tripDao.getOne();//이렇게 해 두면 디비가 바뀔 때마다 trip에 있는 값이 바뀜
+    LiveData<Trip> trip;//이렇게 해 두면 디비가 바뀔 때마다 trip에 있는 값이 바뀜
     //LiveData에 있는 값을 확인하려면 trip.getValue()로 가져옴
     //LiveData는 백그라운드 작업에서 할 필요 없이 알아서 백그라운드 쓰레드로 돌려줘서 쓰레드 처리 할 필요는 없음
+
+    //생성자는 ViewModelProviders.of(this).get(MainViewModel.class);에서 호출해 줌
+    public MainViewModel(){
+        trip = tripDao.getOne();
+    }
 
     void insertNewTrip(final Trip trip){
         //코드는 주구장창 있지만 신경쓸건 적은 백그라운드 쓰레드 실행
